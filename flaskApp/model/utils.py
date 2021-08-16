bb9413792480a31ac335c22e6d4a167eb1e4cfbc
gfelis <felisdedios@gmail.com>
2021-08-16 15:24:08 +0200

    Fixed path

--- flaskApp/model/utils.py
+++ flaskApp/model/utils.py
@@ -4,11 +4,12 @@ import numpy as np
 import tensorflow as tf
 import os
 
-SHOTS_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), os.path.pardir, 'static/shots')
+SHOTS_PATH = SHOTS_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), os.path.pardir, 'static/shots/')
 LABELS_FILE = "labels.csv"
 
 def read_image(image_name: str) -> np.ndarray:
-    return cv2.cvtColor(cv2.imread(SHOTS_PATH + image_name), cv2.COLOR_BGR2RGB)
+    print(SHOTS_PATH + image_name)
+    return cv2.cvtColor(cv2.imread(os.path.join(SHOTS_PATH, image_name)), cv2.COLOR_BGR2RGB)
     
 def load_model(file_name: str)-> tf.keras.Sequential:
     return tf.keras.models.load_model(os.path.join(os.path.dirname(os.path.abspath(__file__)), file_name)) 
